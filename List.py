d3633e3a9d221ad98e10f45d6cc52e93dc44defe
tar <tar@WS-LV-CP4192.local>
2021-12-11 01:29:02 +0200

    added py demo, list files

---
+++ List.py
@@ -0,0 +1,29 @@
+# LIST
+"""values = [1, 2, "rahul", 4, 5]
+print(values[0])
+print(values[-1])
+print(values[1:4])
+values.insert(3, "shetty") # insert "shetty" for index [3], that is after "rahul"
+print(values)
+values.append("THE END")
+print(values)
+values[2] = "RAHUL"
+print(values)
+del values[0]
+print(values)"""
+
+# TUPLE --- is protected (insert, append, del does not work for Tuple)
+"""val = (1,2, "rahul", 4.5)
+print(val[2])"""
+
+# DICTIONARY
+dic = {"a": 2, 4: "abc", "c": "Hello World"}
+print(dic["c"])
+print(dic[4])
+
+diction = {}
+diction["firstname"] = "Rahul"
+diction["lastname"] = "Shetty"
+diction["gender"] = "Male"
+print(diction)
+print(diction["firstname"])
---
+++ demo.py
@@ -0,0 +1,8 @@
+# This is a sample Python script.
+
+print('hello')
+
+a, b, c = 5, 7.2, "World"
+
+print("{} {}".format("Hello", a))
+print(type(a))
