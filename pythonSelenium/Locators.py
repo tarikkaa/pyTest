a7730f1f71a1744ccc977aa837a1221edaf3e3b8
taras.work <testfromtaras@gmail.com>
2021-12-25 23:54:59 +0200

    Added files: demoBrowser.py / and package pythonSelenium

--- pythonSelenium/Locators.py
+++ pythonSelenium/Locators.py
@@ -1,9 +1,10 @@
-# driver = webdriver.Chrome(executable_path="C:\\ChromeDriver\\chromedriver.exe")
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+driver = webdriver.Chrome(executable_path="C:\\ChromeDriver\\chromedriver.exe")
+# driver = webdriver.Chrome(executable_path="/Users/tar/Drivers/chromedriver")
 
-
-driver = webdriver.Chrome(executable_path="/Users/tar/Drivers/chromedriver")
 driver.get("https://rahulshettyacademy.com/angularpractice")
 driver.find_element(By.NAME, "name").send_keys("RAHUL")
-
+driver.find_element(By.ID, "exampleCheck1").click()
+driver.find_element(By.NAME, "email").send_keys("test.email@test.com")
+driver.find_element(By.XPATH, "//input[@type='submit']").click()
