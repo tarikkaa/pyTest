8a27f34f1ad6403af2ad0c1d31e76001a467dca5
Taras <testfromtaras@gmail.com>
2021-12-28 18:34:25 +0200

    Added files: locatorExtensions.py

--- pythonSelenium/Locators.py
+++ pythonSelenium/Locators.py
@@ -1,10 +1,15 @@
+# driver = webdriver.Chrome(executable_path="C:\\ChromeDriver\\chromedriver.exe")
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-driver = webdriver.Chrome(executable_path="C:\\ChromeDriver\\chromedriver.exe")
-# driver = webdriver.Chrome(executable_path="/Users/tar/Drivers/chromedriver")
 
+driver = webdriver.Chrome(executable_path="/Users/tar/Drivers/chromedriver")
 driver.get("https://rahulshettyacademy.com/angularpractice")
 driver.find_element(By.NAME, "name").send_keys("RAHUL")
+driver.find_element(By.NAME, "email").send_keys("email@test.com")
+driver.find_element(By.ID, "exampleInputPassword1").send_keys("password")
+
 driver.find_element(By.ID, "exampleCheck1").click()
-driver.find_element(By.NAME, "email").send_keys("test.email@test.com")
 driver.find_element(By.XPATH, "//input[@type='submit']").click()
+print(driver.find_element(By.CLASS_NAME, "alert-success").text)
+# [class*='alert-success'] - CSS
+# //*[contains(@class, 'alert-success')] - RegEx > xPath
