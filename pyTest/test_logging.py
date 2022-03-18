185c53390fad2b05ff2320b901a6b11b0bcd07a9
Tarik <testfromtaras@gmail.com>
2022-03-18 10:59:36 +0200

    Changes in: test_fixtureData.py, test_logging.py

--- pyTest/test_logging.py
+++ pyTest/test_logging.py
@@ -1,6 +1,17 @@
 import logging
 
-logger = logging.getLogger(__name__)
-logger.debug("A debug statement is executed")
-logger.info("Information statement")
-logger.warning("")
+def test_loggingDemo():
+    logger = logging.getLogger(__name__)
+
+    fileHandler = logging.FileHandler('logfile.log')
+    formatter = logging.Formatter("%(asctime)s :%(levelname)s : %(name)s :%(message)s")
+
+    fileHandler.setFormatter(formatter)
+    logger.addHandler(fileHandler) # filehandler object
+
+    logger.setLevel(logging.INFO)
+    logger.debug("A debug statement is executed")
+    logger.info("Information statement")
+    logger.warning("Something is in warning mode")
+    logger.error("A major error has happened")
+    logger.critical("Critical issue")
